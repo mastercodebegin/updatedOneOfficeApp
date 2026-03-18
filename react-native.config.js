bdb308cc06f9760869df43767b1c80b03cd4e665
Firoz khan <ayankhan1434@gmail.com>
2026-03-18 18:09:16 +0530

    implemented vector icons

--- react-native.config.js
+++ react-native.config.js
@@ -1,3 +1,6 @@
 module.exports = {
-    assets: ['./assets/fonts/'], // Path to the fonts directory
-  };
+  assets: [
+    './assets/fonts/',
+    './node_modules/react-native-vector-icons/Fonts',
+  ],
+};
